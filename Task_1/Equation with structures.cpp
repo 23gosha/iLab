5f36358b686bfc0338e6dbeb2cdf797f21b86c53
23gosha <23gosha@gmail.com>
2014-09-30 19:50:26 +0400

    Indentations

--- Task_1/Equation with structures.cpp
+++ Task_1/Equation with structures.cpp
@@ -3,8 +3,8 @@
 
 struct roots //structure including x_1,x_2 - roots, D - discriminant, count - quantity of roots
 {
-	double D = 0,x_1 = 0,x_2 = 0;
-	int count = 0;
+    double D = 0,x_1 = 0,x_2 = 0;
+    int count = 0;
 };
 
 struct roots function (double a = 0, double b = 0, double c = 0) //function, counting quantity and values of roots
@@ -12,52 +12,52 @@ struct roots function (double a = 0, double b = 0, double c = 0) //function, cou
     struct roots r;
     
     //case à=0 (linear equation)
-	if (a==0)
-	{
-		if (b==0)
-		{
-			if (c==0) r.count=3;
-			if (c!=0) r.count=0;
-		}			
-	
-		if (b!=0)
-		{
-		r.x_1=-c/b;
-		r.count=1;
-	    }
-	}
-	
-	//case a is not equals 3 (quadratic equation)
+    if (a==0)
+    {
+        if (b==0)
+        {
+            if (c==0) r.count=3;
+            if (c!=0) r.count=0;
+        }            
+    
+        if (b!=0)
+        {
+        r.x_1=-c/b;
+        r.count=1;
+        }
+    }
+    
+    //case a is not equals 3 (quadratic equation)
     if (a!=0)
     {
-    	r.D=(b*b)-(4*a*c); //computing discrimninant
+        r.D=(b*b)-(4*a*c); //computing discrimninant
 
         // case of negative discriminant
-    	if (r.D < 0) r.count=0;
-    	// case of zero discriminant
-    	if (r.D == 0)
-    	{
-    		r.count=1;
-    		r.x_1=(-b+sqrt(r.D))/(2*a);
-    	}
-    	// case of positive discriminant
-    	if (r.D > 0)
-    	{
-    		r.count=2;
-    		r.x_1=(-b+sqrt(r.D))/(2*a);
-    		r.x_2=(-b+sqrt(r.D))/(2*a);
-    	}
+        if (r.D < 0) r.count=0;
+        // case of zero discriminant
+        if (r.D == 0)
+        {
+            r.count=1;
+            r.x_1=(-b+sqrt(r.D))/(2*a);
+        }
+        // case of positive discriminant
+        if (r.D > 0)
+        {
+            r.count=2;
+            r.x_1=(-b+sqrt(r.D))/(2*a);
+            r.x_2=(-b+sqrt(r.D))/(2*a);
+        }
     }
     return r;
 }
-	
+    
 
 int main()
 {
-	//INPUT OF VARIABLES
-	roots r;
-	double a = 0, b = 0, c = 0 ;
-	scanf("%lg %lg %lg", &a,&b,&c);
+    //INPUT OF VARIABLES
+    roots r;
+    double a = 0, b = 0, c = 0 ;
+    scanf("%lg %lg %lg", &a,&b,&c);
     
     //COUNTING QUANTITY AND VALUE OF ROOTS
     r = function (a,b,c); // outputting structure e where r.x1,r.x2 - roots, r.n - quantity of roots
@@ -68,5 +68,5 @@ int main()
     if (r.count==2) printf("Two roots: %f and %f",r.x_1,r.x_2);
     if (r.count==3) printf("Any number from R is root");
 
-	return 0;
+    return 0;
 }
